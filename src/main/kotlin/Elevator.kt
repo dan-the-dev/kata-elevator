dc376bdc84277e457065c916d27881b396cf1b59
danielescillia <daniele.scillia@gmail.com>
2021-02-19 02:32:45 +0100

    first attempt just to get confidence with the kata

--- src/main/kotlin/Elevator.kt
+++ src/main/kotlin/Elevator.kt
@@ -1,6 +1,91 @@
 class Elevator {
-    public fun exec(): Boolean
-    {
-        return true
+
+    private var status: ElevatorStatuses = ElevatorStatuses.AWAITING
+
+    private var currentFloor: Int = 0
+
+    fun currentFloor(): Int {
+        return currentFloor
     }
+
+    fun status(): ElevatorStatuses {
+        return status
+    }
+
+    fun call(from: Int, to: Int) {
+        if (from < this.currentFloor) {
+            handleCallFromBelow(from, to)
+            return
+        }
+
+        handleCallFromAbove(from, to)
+    }
+
+    private fun handleCallFromAbove(from: Int, to: Int) {
+        while (currentFloor < from) {
+            this.moveUp()
+        }
+        this.loadPeople()
+        while (currentFloor > to) {
+            this.moveDown()
+        }
+        putInAwaiting()
+        return
+    }
+
+    private fun handleCallFromBelow(from: Int, to: Int) {
+        while (currentFloor > from) {
+            this.moveDown()
+        }
+        this.loadPeople()
+        while (currentFloor < to) {
+            this.moveUp()
+        }
+        putInAwaiting()
+        return
+    }
+
+    private fun putInAwaiting() {
+        setStatus(ElevatorStatuses.AWAITING)
+    }
+
+    private fun loadPeople() {
+        setStatus(ElevatorStatuses.LOADING_PEOPLE)
+        Thread.sleep(3_000)
+    }
+
+    fun moveUp() {
+        setStatus(ElevatorStatuses.GOING_UP)
+        Thread.sleep(1_000)
+        this.goOneFloorUp()
+    }
+
+    fun moveDown() {
+        setStatus(ElevatorStatuses.GOING_DOWN)
+        Thread.sleep(1_000)
+        this.goOneFloorDown()
+    }
+
+    private fun setStatus(newStatus: ElevatorStatuses) {
+        this.status = newStatus
+    }
+
+    private fun goOneFloorUp() {
+        this.setCurrentFloor(this.currentFloor+1)
+    }
+
+    private fun goOneFloorDown() {
+        this.setCurrentFloor(this.currentFloor-1)
+    }
+
+    private fun setCurrentFloor(newFloor: Int) {
+        this.currentFloor = newFloor
+    }
+}
+
+enum class ElevatorStatuses(val status: String) {
+    AWAITING("awaiting"),
+    GOING_UP("going_up"),
+    GOING_DOWN("going_down"),
+    LOADING_PEOPLE("loading_people")
 }
--- src/test/kotlin/ElevatorTest.kt
+++ src/test/kotlin/ElevatorTest.kt
@@ -1,19 +1,73 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.concurrent.thread
 
-internal class ElevatorTest() {
+internal class ElevatorTest {
 
     @Test
-    fun testShallPass(): Unit
+    fun testElevatorStartsAtGroundFloorWithStatusAwaiting()
     {
-        assertEquals(1, 1)
+        val elevator = Elevator()
+
+        assertEquals(0, elevator.currentFloor())
+        assertEquals(ElevatorStatuses.AWAITING, elevator.status())
     }
 
     @Test
-    fun testHandleReturnTrue(): Unit
+    fun testElevatorChangeStatusToGoingUpWhenCalledFromFloor1()
     {
-        val kata = Elevator()
-        assertEquals(true, kata.exec())
+        val elevator = Elevator()
+
+        thread {
+            elevator.call(1, 0)
+        }
+
+        Thread.sleep(100)
+        assertEquals(ElevatorStatuses.GOING_UP, elevator.status())
+        Thread.sleep(1_000)
+        assertEquals(ElevatorStatuses.LOADING_PEOPLE, elevator.status())
+        Thread.sleep(3_000)
+        assertEquals(ElevatorStatuses.GOING_DOWN, elevator.status())
+        Thread.sleep(1_000)
+        assertEquals(ElevatorStatuses.AWAITING, elevator.status())
+    }
+
+    @Test
+    fun testElevatorChangeStatusToGoingDownWhenCalledFromBasement()
+    {
+        val elevator = Elevator()
+
+        thread {
+            elevator.call(-1, 0)
+        }
+
+        Thread.sleep(100)
+        assertEquals(ElevatorStatuses.GOING_DOWN, elevator.status())
+        Thread.sleep(1_000)
+        assertEquals(ElevatorStatuses.LOADING_PEOPLE, elevator.status())
+        Thread.sleep(3_000)
+        assertEquals(ElevatorStatuses.GOING_UP, elevator.status())
+        Thread.sleep(1_000)
+        assertEquals(ElevatorStatuses.AWAITING, elevator.status())
+    }
+
+    @Test
+    fun testElevatorGoesFrom3toBasementCorrectly()
+    {
+        val elevator = Elevator()
+
+        thread {
+            elevator.call(3, -1)
+        }
+
+        Thread.sleep(500)
+        assertEquals(ElevatorStatuses.GOING_UP, elevator.status())
+        Thread.sleep(3_000)
+        assertEquals(ElevatorStatuses.LOADING_PEOPLE, elevator.status())
+        Thread.sleep(3_000)
+        assertEquals(ElevatorStatuses.GOING_DOWN, elevator.status())
+        Thread.sleep(4_000)
+        assertEquals(ElevatorStatuses.AWAITING, elevator.status())
     }
 
 }
